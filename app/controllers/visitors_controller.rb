9b566102ecffc2c7a6f20f7fc896251a1bb6b23a
Daniel Bonnell <daniel.bonnell@gmail.com>
2016-04-14 12:20:11 -0400

    embed test

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,4 +1,13 @@
 class VisitorsController < ApplicationController
-  def widget
-  end
+    def widget
+    end
+
+    def instagram
+    end
+
+    def twitter
+    end
+
+    def facebook
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   get '/widget' => 'visitors#widget'
+  get '/instagram' => 'visitors#instagram'
+  get '/twitter' => 'visitors#twitter'
+  get '/facebook' => 'visitors#facebook'
 end
